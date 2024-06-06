4837619bf58bf25b0ee4aead1101e37983ce5f94
Robson <80903521+Robson47@users.noreply.github.com>
2024-06-05 22:16:14 -0300

    changes

--- server/budget/controller/budgetController.js
+++ server/budget/controller/budgetController.js
@@ -12,14 +12,38 @@ exports.createBudget = async (req, res) => {
         return res.status(500).json({ msg: 'Erro no servidor!' })
     };
 };
+ 
+exports.updateBudget = async (req, res) => {
+    const id = req.headers.id;
+    const { problem_type, problem_details, value, device_model, status } = req.body;
+    const budget = { problem_type, problem_details, value, device_model, status };
 
+    try {
+        const updatedBudget = await Budget.updateOne({ _id: id }, budget);
 
+        if (updatedBudget.matchedCount === 0) {
+            return res.status(404).json({ msg: 'Orçamento não encontrado!' });
+        } else {
+            res.status(201).json({ msg: `O orçamento ${updatedBudget} foi alterado com sucesso!` });
+        };
+    } catch (error) {
+        res.status(500).json({ msg: error });
+    };
+}
 
 exports.deleteBudget = async (req, res) => {
-    try {
-        const { problem_type, problem_details, value, device_model } = req.body;
-        
-    } catch (error) {
-        return res.status(500).json({ msg: 'Erro no servidor!' })
+    const id = req.headers.id;
+    const budget = await Budget.findOne({ _id: id });
+
+    if (!budget) {
+        return res.status(404).json({ msg: 'Orçamento não encontrado!' });
     };
+
+    try {
+        await Budget.deleteOne({ _id: id });
+        res.status(200).json({ msg: 'O orçamento foi deletado com sucesso!' });
+    } catch (error) {
+        res.status(500).json({ msg: error });
+    };
+
 };
